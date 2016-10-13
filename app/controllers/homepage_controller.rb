90b43eba9e52f4ee27a7ca1d8ea2e62ffd821e9c
Adam Fronczak <Fronczak.Adam@gmail.com>
2016-10-13 14:33:48 -0400

    controller method name corrected

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,5 @@
 class HomepageController < ApplicationController
-  def render_pets
+  def index
     petfinder = Petfinder::Client.new('9f7e499c0441264a4cfea0242c9b005d', '44c67bd61a8f799abc055c9f367397f6')
     shelter = petfinder.shelter('FL697')
     url = HTTParty.get("http://api.petfinder.com/my.method?key=d6af2d543aed1b6d27ab54b3d4b76815=5a3619c14d603b86b4b16e4da363407e")
