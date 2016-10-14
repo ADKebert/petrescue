9019e7421610b71b0c3b5b1c2b1ab299a68ec85b
Alan Kebert <adkebert@yahoo.com>
2016-10-14 08:47:08 -0400

    It was already working but see the example for how to get useful info out of it

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,9 +2,10 @@ class HomepageController < ApplicationController
   def index
     petfinder = Petfinder::Client.new('9f7e499c0441264a4cfea0242c9b005d', '44c67bd61a8f799abc055c9f367397f6')
     shelter = petfinder.shelter('FL697')
-    url = HTTParty.get("http://api.petfinder.com/my.method?key=d6af2d543aed1b6d27ab54b3d4b76815=5a3619c14d603b86b4b16e4da363407e")
-
-    http://api.petfinder.com/auth.getToken?key=9f7e499c0441264a4cfea0242c9b005d&sig=a1e5de97c9636f487f803537baabf857
+    pets = petfinder.shelter_pets('FL697')
+    # It's working, you just need to get the stuff out of the pet object for it to render anything.
+    # The method's I've found for a pet on the github are: :id, :name, :animal, :mix, :age, :shelter_id, :shelter_pet_id, :sex, :size, :description, :last_update, :status
+    render json: pets.map(&:inspect)
   end
 end
 
